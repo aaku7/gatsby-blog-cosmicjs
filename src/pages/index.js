cb190755fcdc41d2d90c1392e97f9de8c3b275d1
aaku7 <97825204+aaku7@users.noreply.github.com>
2022-01-16 14:24:54 +0530

    updated title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ class BlogIndex extends React.Component {
   render() {
     const siteTitle = get(
       this,
-      'props.data.cosmicjsSettings.metadata.site_title'
+      'Akshada Blog'
     )
     const posts = get(this, 'props.data.allCosmicjsPosts.edges')
     const author = get(this, 'props.data.cosmicjsSettings.metadata')
